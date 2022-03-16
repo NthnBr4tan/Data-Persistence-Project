4c9c577f565348345aef62ed0115b581f57fb28b
NthnBr4tan <101659860+NthnBr4tan@users.noreply.github.com>
2022-03-16 22:54:12 +0100

    Coding Challenge
    Coding the text input field

---
+++ Assets/Scripts/MainManager2.cs
@@ -0,0 +1,20 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MainManager2 : MonoBehaviour
+{
+    public static MainManager2 Instance;
+
+    public string Username;
+    private void Awake()
+    {
+        if (Instance != null)
+        {
+            Destroy(gameObject);
+            return;
+        }
+        Instance = this;
+        DontDestroyOnLoad(gameObject);
+    }
+}
---
+++ Assets/Scripts/MenuUIHandler.cs
@@ -0,0 +1,31 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEditor;
+using UnityEngine.UI;
+
+
+public class MenuUIHandler : MonoBehaviour
+{
+    [SerializeField] InputField Username;
+    public void LoadScene()
+    {
+        SceneManager.LoadScene(1);
+    }
+
+    public void Exit()
+    {
+#if UNITY_EDITOR 
+        EditorApplication.ExitPlaymode();
+#else
+        Application.Quit();
+#endif
+    }
+
+    public void NameSelected()
+    {
+        MainManager2.Instance.Username = Username.text;
+    }
+    
+}
